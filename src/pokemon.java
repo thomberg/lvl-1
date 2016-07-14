85529e42288f1e38687d56eea4339e9dd3ff6546
thomberg <league@Mac-mini-2.local>
2016-07-14 16:49:17 -0700

    fsdfadsrf
    dsfsadfdsafsdaf

---
+++ src/pokemon.java
@@ -0,0 +1,23 @@
+
+public class pokemon {
+String type;
+String name;
+boolean hasTail;
+
+void attack(){
+System.out.println(name + " is attacking");	
+	
+	
+	
+}
+
+public pokemon(String type, String name, boolean hasTail) {
+	super();
+	this.type = type;
+	this.name = name;
+	this.hasTail = hasTail;
+}
+
+}
+
+
---
+++ src/pokemonmaker.java
@@ -0,0 +1,9 @@
+
+public class pokemonmaker {
+public static void main(String[] args) {
+	pokemon diglet = new pokemon("ground", "diglet", false);
+	diglet.attack();
+}
+	pokemon mew = new pokemon("psychic","mew",true );
+	
+}
